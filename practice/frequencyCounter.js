ea2881d2ff965171afc76fd790426be1cd0e8836
HuffDaddy0 <ty.huff24@gmail.com>
2021-03-06 16:01:00 -0500

    frequency count practice

--- practice/frequencyCounter.js
+++ practice/frequencyCounter.js
@@ -51,3 +51,29 @@ function sameRefact(arr1, arr2){
         return true
     }
 }
+
+//! given 2 strings find if they are anagrams
+//! Anagram - a word formed by rearranging the
+//! letters of another
+
+function validAnagram(str1, str2){
+    if(str1.length !== str2.length){
+        return false
+    }
+    let count1 = {}
+    let count2 = {}
+    for(let char of str1){
+        count1[char] = (count1[char] || 0) + 1
+    }
+    for(let char of str2){
+        count2[char] = (count2[char] || 0) + 1
+    }
+    console.log(count1)
+    for(let key in count1){
+        console.log(key)
+        console.log(count1[key])
+        if((count2[key] !== count1[key]))
+        return false
+    }
+    return true
+  }
