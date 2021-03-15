9802c25797d380a08b97fc94215f9b605dcad1ca
HuffDaddy0 <ty.huff24@gmail.com>
2021-03-15 02:20:55 -0400

    recursion basics

--- practice/recursion.js
+++ practice/recursion.js
@@ -1,1 +1,21 @@
-cd
+//Basic count down recursion:
+
+function countDown(num){
+    if(num <=0){
+        console.log("All Done!")
+        return
+    }
+    console.lof(num)
+    num--
+    countDown(num)
+}
+
+function sumRange(num){
+    if (num=== 1) return 1
+    return num + sumRange(num-1)
+}
+
+function factorial(num){
+if(num === 1) return 1
+return num * factorial(num-1)
+}
